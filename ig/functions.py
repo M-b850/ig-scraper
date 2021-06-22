6d30c556c691f60ab85bdd4fec6224f93458c687
Mehrshad Baqerzadegan <mehrshadbaqerzadegan@gmail.com>
2021-06-22 19:12:04 +0430

    Change The Update database.

--- ig/functions.py
+++ ig/functions.py
@@ -6,14 +6,13 @@ from os.path import dirname, abspath
 
 
 DIR = dirname(dirname(abspath(__file__)))
-res = []
 
-def file_name(realse_date: datetime):
+def file_name(realse_date):
     suffix = '_'
     realse_date = realse_date.replace(':', '_').replace(' ', '_')
     return realse_date
 
-def get_data(L, db: object, inst_username: str) -> list[dict]:
+def get_data(L, db, inst_username):
     print(inst_username)
     profile = Profile.from_username(L.context, inst_username)
 
@@ -21,40 +20,39 @@ def get_data(L, db: object, inst_username: str) -> list[dict]:
     PostFolowingPostShare = profile.followees
     PostCount = profile.mediacount
     all_posts = profile.get_posts()
+
     for one_post in all_posts:
-        each_post = {
-            'InfoUpdateDate': datetime.datetime.utcnow(),
-            'InsPageLink': const.IG_PROFILE + inst_username,
-            'PostFolowerPostShare': PostFolowerPostShare,
-            'PostFolowingPostShare': PostFolowingPostShare,
-            'PostCount': PostCount,
-            'RelaseDate': one_post.date_utc,
-            'InsPostlink': const.IG_URL + one_post.shortcode,
-            'PostImagelink': one_post.url,
-            'PostLike': one_post.likes,
-            'PostComment': one_post.comments,
-            'PostSaveCount': None,
-            'PostSendCount': None,
-            'PostDiscription': one_post.caption,
-        }
-        _file_name = file_name(str(each_post['RelaseDate'])) + \
-                    '_UTC' + f'_{inst_username}'
-        image_address = f'media/{_file_name}'
-        L.download_pic(
-            image_address,
-            each_post['PostImagelink'],
-            each_post['RelaseDate'],
-        )
-        each_post['PostImagelink'] = f'{DIR}/' + image_address + '.jpg'
-        
-        # Insert to Database
+        InsPostlink = const.IG_URL + one_post.shortcode
         """
-        If object exists it would be found and be deleted after that -
-        it will be inserted again.
+        If object doesn't exists it will be added to database.-
+        Other ways it won't.
         """
-        filter = {'InsPostlink': each_post['InsPostlink']}
+        filter = {'InsPostlink': InsPostlink}
         if not db.find_one(filter):
-            db.insert_one(each_post)
-        
-        res.append(each_post)
-    return res
+            each_post = {
+                'InfoUpdateDate': datetime.datetime.utcnow(),
+                'InsPageLink': const.IG_PROFILE + inst_username,
+                'PostFolowerPostShare': PostFolowerPostShare,
+                'PostFolowingPostShare': PostFolowingPostShare,
+                'PostCount': PostCount,
+                'RelaseDate': one_post.date_utc,
+                'InsPostlink': InsPostlink,
+                'PostImagelink': one_post.url,
+                'PostLike': one_post.likes,
+                'PostComment': one_post.comments,
+                'PostSaveCount': None,
+                'PostSendCount': None,
+                'PostDiscription': one_post.caption,
+            }
+            _file_name = file_name(str(each_post['RelaseDate'])) + \
+                        '_UTC' + f'_{inst_username}'
+            image_address = f'media/{_file_name}'
+            L.download_pic(
+                image_address,
+                each_post['PostImagelink'],
+                each_post['RelaseDate'],
+            )
+            each_post['PostImagelink'] = f'{DIR}/' + image_address + '.jpg'
+            
+            db.insert_one(each_post)  # Insert to Database
+    return True

2588a0eb7d05d573a11ceadbf24941837513903f
Mehrshad Baqerzadegan <mehrshadbaqerzadegan@gmail.com>
2021-06-30 03:19:42 +0430

    Fix bugs.

--- ig/functions.py
+++ ig/functions.py
@@ -3,6 +3,7 @@ import datetime
 from os.path import dirname, abspath
 import random
 import time
+from bson import Int64
 from itertools import dropwhile, takewhile
 
 from instaloader import Instaloader, Profile
@@ -25,7 +26,7 @@ def get_comments(db, post):
     db.comments_col()
     comments = post.get_comments()    
     for c in comments:
-        filter = {'id': c.id}
+        filter = {'id': Int64(c.id)}
         if not db.find_one(filter):
             comment = {
                 'id': c.id,
@@ -36,12 +37,8 @@ def get_comments(db, post):
                 'CommentDate': c.created_at_utc,
                 'CommetDescription': c.text,
                 'CommentLike': c.likes_count,
-                'ReplyCount': None,
+                'ReplyCount': sum(1 for _ in comments) + 1,
             }
-            count = 0
-            for answer in c.answers:
-                count+=1
-            comment['ReplyCount'] = count
             db.insert_one(comment)
 
 def get_data(L, db, inst_username):    
@@ -56,13 +53,16 @@ def get_data(L, db, inst_username):
     # for one_post in all_posts:
         InsPostlink = const.IG_URL + one_post.shortcode
         get_comments(db, one_post)
-        break
         """
         If object doesn't exists it will be added to database.-
         Other ways it won't.
         """
-        filter = {'InsPostlink': InsPostlink}
+        db.posts_col()
+
+        filter = {'InsPostlink': str(InsPostlink)}
+
         if not db.find_one(filter):
+            print(filter)
             # Sleeep
             # insomnia = random.randint(1, 4)
             # print('\n~~~~Post Insomnia is:', insomnia)
@@ -94,17 +94,16 @@ def get_data(L, db, inst_username):
             )
             each_post['PostImagelink'] = f'{DIR}/' + image_address + '.jpg'
             
-            db.posts_col()
-            # db.insert_one(each_post)  # Insert to Database
+            db.insert_one(each_post)  # Insert to Database
 
 
     res = {
         'InsPageLink': const.IG_PROFILE + inst_username,
         'InsPageName': inst_username,
-        'BioText': None,
+        'BioText': profile.biography,
         'FolowerAtUpdate': PostFolowerPostShare,
         'FolowingAtUpdate': PostFolowingPostShare,
         'PostCount': sum(1 for _ in all_posts) + 1,
-        'SiteLink': None
+        'SiteLink': profile.external_url
     }
     return res
